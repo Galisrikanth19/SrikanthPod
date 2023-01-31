1e7e7752cfad98bab2f5197bc56838e0f24aa83f
Srikanth <srikanth@webappclouds.com>
2023-01-31 18:27:23 +0530

    Added simple pod

---
+++ SwiftyLib.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftyLib.swift
+//  SrikanthPod
+//
+//  Created by Webappclouds on 31/01/23.
+//
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
