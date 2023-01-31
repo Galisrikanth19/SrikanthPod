f0080715fa4b1daef85d85272bc584c3313faa1f
Srikanth <srikanth@webappclouds.com>
2023-01-31 18:15:56 +0530

    Initial Commit

---
+++ SrikanthPod/SrikanthPod.h
@@ -0,0 +1,18 @@
+//
+//  SrikanthPod.h
+//  SrikanthPod
+//
+//  Created by Webappclouds on 31/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SrikanthPod.
+FOUNDATION_EXPORT double SrikanthPodVersionNumber;
+
+//! Project version string for SrikanthPod.
+FOUNDATION_EXPORT const unsigned char SrikanthPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SrikanthPod/PublicHeader.h>
+
+
